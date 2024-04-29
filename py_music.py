8e43a75d9c94a3fb8b036164ec174d06fd61d03f
Brian3-21 <brianperezdota@gmail.com>
2024-04-29 01:59:34 -0400

    fix ventana size

--- py_music.py
+++ py_music.py
@@ -1,13 +1,40 @@
+import tkinter as tk
 from pytube import YouTube
+import humanize
 
-# URL del video de YouTube
-url = 'https://www.youtube.com/watch?v=lc1Xaf6VVCw'
+#ejemplo de limite de edad
+#https://www.youtube.com/watch?v=aezstCBHOPQ
 
-# Crear objeto YouTube
-video = YouTube(url)
+def descargar_audio():
+    url = entry_url.get()
+    try:
+        video = YouTube(url)
+        audio = video.streams.filter(only_audio=True).first()
+        file_size = audio.filesize
+        file_size_readable = humanize.naturalsize(file_size)
+        label_status.config(text=f"Tamaño del archivo: {file_size_readable}")
+        audio.download(output_path='ruta_de_guardado')
+        label_status.config(text="Audio descargado exitosamente.")
+    except Exception as e:
+        label_status.config(text="Error al descargar el audio.")
+        print(e)
 
-# Descargar el audio en la mejor calidad disponible
-audio = video.streams.filter(only_audio=True).first()
-audio.download(output_path='ruta_de_guardado')
+# Crear la ventana principal
+ventana = tk.Tk()
+ventana.title("Descargador de audio de YouTube")
+ventana.minsize(250, 100)
 
-print("Audio descargado exitosamente.")
+# Crear los widgets
+label_url = tk.Label(ventana, text="Ingresa la URL del video:")
+entry_url = tk.Entry(ventana)
+boton_descargar = tk.Button(ventana, text="Descargar audio", command=descargar_audio)
+label_status = tk.Label(ventana, text="")
+
+# Organizar los widgets en la ventana
+label_url.pack()
+entry_url.pack()
+boton_descargar.pack()
+label_status.pack()
+
+# Iniciar el bucle principal de la aplicación
+ventana.mainloop()
