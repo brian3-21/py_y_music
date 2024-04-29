df0e0c296b9f0e61538dae3942adc715a6d05454
Brian3-21 <brianperezdota@gmail.com>
2024-04-29 01:31:46 -0400

    add gitignore

--- py_music.py
+++ py_music.py
@@ -1,1 +1,13 @@
-print(" hola wiri")
+from pytube import YouTube
+
+# URL del video de YouTube
+url = 'https://www.youtube.com/watch?v=lc1Xaf6VVCw'
+
+# Crear objeto YouTube
+video = YouTube(url)
+
+# Descargar el audio en la mejor calidad disponible
+audio = video.streams.filter(only_audio=True).first()
+audio.download(output_path='ruta_de_guardado')
+
+print("Audio descargado exitosamente.")
